e4396270bdf41a85d7310ce7db983a1f3711685f
t3llo <lolwtfxd49@gmail.com>
2020-02-21 18:59:39 -0500

    Estructuracion

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Facts from './Containers/Facts'
+import Chicharron from './Components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Chicharron/>
+      <Facts/>
+      
+    </>
+  );
+}
+
+export default App;
