674e8542389a13aaff156f948154104f0a8df690
Maria Antonia Rincon <marinconz@eafit.edu.co>
2020-03-11 15:25:40 -0500

    Agregando más dependencias

--- app/src/containers/CameraView/index.js
+++ app/src/containers/CameraView/index.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function CamaraScreen() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Modulo de camara!</Text>
+      <Text>Take picture</Text>
     </View>
   );
 }
