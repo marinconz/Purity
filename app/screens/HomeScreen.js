379ddaf493d7483b2b634ee078f4fc8cd98b8d98
Maria Antonia Rincon <marinconz@eafit.edu.co>
2020-03-17 17:18:25 -0500

    Vista del catálogo sin info real

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, FlatList, ShadowPropTypesIOS } from 'react-native';
+
+export default function HomeScreen() {
+  
+    const list = [{key:'Ingrediente 0', des: 'Toxic'},
+    {key:'Ingrediente 1', des: 'Toxic'},
+    {key:'Ingrediente 2', des: 'Toxic'},
+    {key:'Ingrediente 3', des: 'Toxic'},
+    {key:'Ingrediente 4', des: 'Toxic'},
+    {key:'Ingrediente 5', des: 'Toxic'},
+    {key:'Ingrediente 6', des: 'Toxic'},
+    {key:'Ingrediente 7', des: 'Toxic'},
+    {key:'Ingrediente 8', des: 'Toxic'},
+  ];
+  
+    return (
+      <View style={styles.container}>
+        
+        { list.length > 0 ?
+          <FlatList data={list}
+          renderItem={({item})=> (
+            <View style = {{borderBottomColor: '#999', padding: 10}}>
+            <Text style = {{fontSize: 50, fontWeight: 'bold', color:'#333'}}>
+              {item.key}
+            </Text>
+            <Text style={{fontSize:36, color: '#999'}}>
+              {item.des}
+            </Text>
+            </View>
+
+          ) }/>
+          :
+
+          <Text style = {{fontSize:48, color: 'red'}}>
+            Sorry, no ingredients avaliable.
+          </Text>
+        }
+      </View>
+    );
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  listitem: {
+    fontSize:45,
+    fontWeight: 'bold',
+    color: '#333',
+    borderBottomWidth: 2,
+    borderBottomColor: '#999'
+  }
+});
+
